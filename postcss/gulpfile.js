4415882040537673b00cd5d697aacce9867ee770
huwei <2570328000@qq.com>
2018-03-06 11:08:43 +0800

    代码提交

---
+++ postcss/gulpfile.js
@@ -0,0 +1,81 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var cssnano = require('cssnano');
+var precss = require('precss');
+var cssnext = require('cssnext');
+var stylelint = require('stylelint'); 
+var reporter = require('postcss-reporter');
+var cssModules = require('postcss-modules');
+var ejs = require('gulp-ejs');
+var fs = require('fs');
+var path = require('path');
+
+function getJSONFromCssModules(cssFileName, json) {
+  const cssName       = path.basename(cssFileName, '.css');
+  const jsonFileName  = path.resolve('dest/', `${ cssName }.json`);
+  fs.writeFileSync(jsonFileName, JSON.stringify(json));
+};
+
+function getClass(module, className) {
+  const moduleFileName  = path.resolve('dest/', `${ module }.json`);
+  const classNames      = fs.readFileSync(moduleFileName).toString();
+  return JSON.parse(classNames)[className];
+};
+
+gulp.task('styles',function(){
+	return gulp.src('src/css/*.css')
+		.pipe(postcss([
+			autoprefixer,
+			precss,
+			cssnext,
+			cssModules({ getJSON: getJSONFromCssModules }),
+		]))
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.write('maps/'))
+		.pipe(gulp.dest('dest/css'));
+});
+
+gulp.task('rename', ['styles'], function(){
+	return gulp.src('dest/css/style.css')
+		.pipe(postcss([
+			cssnano,
+		]))
+		.pipe(rename('style.min.css'))
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.write('maps/'))
+		.pipe(gulp.dest('dest/css'));
+});
+
+gulp.task('html', ['styles'], function(){
+	return gulp.src('app/**/*.ejs')
+		.pipe(ejs({ className: getClass }, {}, { ext: '.html' }))
+		.pipe(gulp.dest('dest/'));
+});
+
+// gulp.task("lint-styles", function() {
+// 	return gulp.src("src/css/*.css")
+// 	.pipe(postcss([ 
+// 		stylelint({
+// 			"rules": {
+// 				"color-no-invalid-hex": true,
+// 				"declaration-colon-space-before": [2, "never"],
+// 				"indentation": [2, 2],
+// 				"number-leading-zero": [2, "always"]
+// 			}
+// 		}),
+// 		reporter({ 
+// 			clearMessages: true,
+// 		})
+// 	]))
+// });
+
+// gulp.task('default', ['lint-styles', 'styles', 'rename'])
+gulp.task('default', ['html', 'styles', 'rename'])
+
+var watcher = gulp.watch('src/css/*.css', ['default']);
+watcher.on('change', function(event) { 
+	console.log('File ' + event.path + ' was ' + event.type + ', running tasks...'); 
+});
